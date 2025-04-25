be3e8ac7cb1a6adfc31db36378d709ff981a1ed0
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-04-25 20:54:21 +0300

    add RafikeyBubble

---
+++ rafikey-frontend/src/views/chatbot/ChatPage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import RafikeyBubble from '@/components/bubble/RafikeyBubble.vue'
+import UserBubble from '@/components/bubble/UserBubble.vue'
+</script>
+
+<template>
+  <p>Chat Page</p>
+  <RafikeyBubble />
+  <UserBubble />
+
+</template>
+
+<style scoped>
+
+</style>
