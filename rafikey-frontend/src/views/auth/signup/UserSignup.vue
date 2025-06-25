07596b81137b2e52afa97258df6d1037c46ba7b6
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-06-25 22:29:14 +0300

    add UserSignup RouterView

---
+++ rafikey-frontend/src/views/auth/signup/UserSignup.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>
+  <RouterView #default="{Component, route}">
+    <template v-if="Component">
+      <component :is="Component" :key="route.fullPath"/>
+    </template>
+  </RouterView>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
