cc938a46a467d5e5a00b850845ad4c4a6686e680
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-06-25 22:19:07 +0300

    add alert.ts module for alerts

---
+++ rafikey-frontend/src/modules/alert.ts
@@ -0,0 +1,46 @@
+import Swal from 'sweetalert2'
+
+interface SweetAlertType {
+  type: 'success' | 'error' | 'warning' | 'info'
+  message: string
+}
+
+export const showSweetAlert= (alertPayload: SweetAlertType )=>{
+  if(alertPayload.type === 'success'){
+    Swal.fire({
+      title: 'Success',
+      text: alertPayload.message,
+      icon: 'success',
+      confirmButtonText: 'Okay',
+      timer: 3000
+    })
+  }
+
+  else if(alertPayload.type === 'error'){
+    Swal.fire({
+      title: 'Error',
+      text: alertPayload.message,
+      icon: 'error',
+      confirmButtonText: 'Okay',
+      timer: 3000
+    })
+  }
+  else if(alertPayload.type === 'info') {
+    Swal.fire({
+      title: 'Info',
+      text: alertPayload.message,
+      icon: 'info',
+      confirmButtonText: 'Okay',
+      timer: 3000
+    })
+  }
+  else{
+    Swal.fire({
+      title: 'warning',
+      text: alertPayload.message,
+      icon: 'warning',
+      confirmButtonText: 'Okay',
+      timer: 3000
+    })
+  }
+}
