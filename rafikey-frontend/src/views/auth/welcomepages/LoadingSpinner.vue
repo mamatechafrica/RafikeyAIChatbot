bc277a4230e2976f3fd2a6749d61e98a7cf68bcb
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-06-25 22:22:28 +0300

    add LoadingSpinner Page

---
+++ rafikey-frontend/src/views/auth/welcomepages/LoadingSpinner.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex  flex-col  gap-4 items-center justify-center h-screen w-full bg-white">
+    <div class="">
+      <img class="h-20 lg:h-32" src="@/assets/images/rafikey-bot.png" alt="rafikey--bot"/>
+    </div>
+    <div class="flex space-x-2">
+      <div class="w-8 h-3 rounded-md animate-pulse dark:bg-casablanca-400"></div>
+      <div class="w-3 h-3 rounded-full animate-pulse dark:bg-casablanca-200"></div>
+      <div class="w-3 h-3 rounded-full animate-pulse dark:bg-casablanca-300"></div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
