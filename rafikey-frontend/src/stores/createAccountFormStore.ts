e316878415809b36848e360fcc244805daa95d40
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-06-25 22:20:43 +0300

    add createAccountFormStore

---
+++ rafikey-frontend/src/stores/createAccountFormStore.ts
@@ -0,0 +1,33 @@
+import { defineStore} from "pinia";
+import type {CreateAccountPayload} from "@/stores/authStore.ts";
+
+export const useCreateAccountFormStore = defineStore("createAccountFormStore", {
+  // id: 'createAccountFormStore',
+  state: () =>({
+    profile: {
+      username: '',
+      email: '',
+      password: '',
+      age: '',
+      gender: '',
+      relationship_status:'',
+      terms_accepted: false
+    }
+  }),
+
+  getters: {
+    getProfile: (state) => state.profile,
+  },
+
+  actions: {
+    setProfile(value: Partial<CreateAccountPayload>){
+      this.profile = {
+        ...this.profile,
+        ...value
+      }
+    },
+    clearProfile(){
+      this.$reset()
+    }
+  }
+})
