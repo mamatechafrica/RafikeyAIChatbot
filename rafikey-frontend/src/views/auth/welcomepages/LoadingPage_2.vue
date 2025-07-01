be064ed67bd136757d0e32096a27c43dfc481634
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-01 11:05:41 +0300

    get darkMode from localstorage

--- rafikey-frontend/src/views/auth/welcomepages/LoadingPage_2.vue
+++ rafikey-frontend/src/views/auth/welcomepages/LoadingPage_2.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+import imageLight from '../../../assets/images/rafikey-icon.png'
+import imageDark from '../../../assets/images/rafikey-icon-black.png'
+import { computed  } from 'vue'
 
-import LoadingSpinner from '@/views/auth/welcomepages/LoadingSpinner.vue'
+const isDark = localStorage.getItem('darkMode')
+console.log("we have mode", isDark)
+const toggleImage = computed(()=>{
+  return isDark? imageDark : imageLight
+})
 </script>
 
 <template>
