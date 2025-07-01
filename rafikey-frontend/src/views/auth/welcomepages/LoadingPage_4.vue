435a05e167046e18c67399867eb8c7a5bcc346d1
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-01 11:09:13 +0300

    add darkmode styling

--- rafikey-frontend/src/views/auth/welcomepages/LoadingPage_4.vue
+++ rafikey-frontend/src/views/auth/welcomepages/LoadingPage_4.vue
@@ -13,19 +13,24 @@
 </script>
 
 <template>
-  <div class="h-screen w-full space-y-5">
+  <div class="h-screen w-full space-y-5 dark:bg-lightgray ">
     <div class="flex w-full justify-center">
-      <img src="@/assets/images/rafikey-icon.png" alt="rafikey-logo-image" />
+      <img :src="toggleImage" alt="rafikey-logo-image"  class="invisible lg:visible" />
     </div>
-    <div class="flex flex-col items-center mx-auto w-10/12 space-y-12 ">
+    <div class="flex flex-col items-center py-10 mx-auto lg:w-8/12 xl:w-6/12  w-10/12 space-y-12 bg-lightBackground dark:bg-darkgray rounded-2xl">
       <div class="ps-10">
-        <p class="lg:text-3xl text-2xl font-semibold">Private & Safe:</p>
+        <p class="text-3xl  font-semibold dark:text-white">Private & Safe:</p>
       </div>
       <div>
         <img src="@/assets/images/rafikey-lock.png" alt="rafikey-lock image" />
       </div>
-      <div class="w-4/12">
-        <p class="lg:text-xl text-lg text-gray-700  text-center flex justify-center ">Everything you share is protected and stays between you and Rafikey</p>
+      <div class="xl:w-6/12 w-9/12">
+        <p class="lg:text-xl text-lg dark:text-white text-gray-700  text-center flex justify-center ">Everything you share is protected and stays between you and Rafikey</p>
+      </div>
+      <div class="flex space-x-2">
+        <div class="w-8 h-3 rounded-md animate-pulse dark:bg-casablanca-400"></div>
+        <div class="w-3 h-3 rounded-full animate-pulse dark:bg-casablanca-200"></div>
+        <div class="w-3 h-3 rounded-full animate-pulse dark:bg-casablanca-300"></div>
       </div>
 
     </div>
