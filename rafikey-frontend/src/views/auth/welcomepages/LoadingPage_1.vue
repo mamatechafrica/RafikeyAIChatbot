bf79cd2eeab0404dcffbb879c90e5e715994d371
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-01 11:05:32 +0300

    get darkMode from localstorage

--- rafikey-frontend/src/views/auth/welcomepages/LoadingPage_1.vue
+++ rafikey-frontend/src/views/auth/welcomepages/LoadingPage_1.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+import imageLight from '../../../assets/images/rafikey-icon.png'
+import imageDark from '../../../assets/images/rafikey-icon-black.png'
+import { computed  } from 'vue'
 
+const isDark = localStorage.getItem('darkMode')
+console.log("we have mode", isDark)
+const toggleImage = computed(()=>{
+  return isDark? imageDark : imageLight
+})
 </script>
 
 <template>
-  <div class="h-screen w-full">
-    <div class="flex justify-center items-center h-full">
-      <img src="../../../assets/images/rafikey-icon.png" alt="rafikey-icon"/>
+  <div class="h-screen w-full ">
+    <div class="flex justify-center items-center h-full dark:bg-darkgray ">
+      <img :src="toggleImage" alt="rafikey-icon"/>
     </div>
   </div>
 </template>
