b183af4ad09320a79f5c27f1be90b532d6b7362e
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-01 11:10:12 +0300

    add styling

--- rafikey-frontend/src/views/auth/welcomepages/LoadingSpinner.vue
+++ rafikey-frontend/src/views/auth/welcomepages/LoadingSpinner.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-  <div class="flex  flex-col  gap-4 items-center justify-center h-screen w-full bg-white">
+  <div class="flex  flex-col  gap-4 items-center justify-center h-screen w-full bg-white dark:bg-lightgray">
     <div class="">
-      <img class="h-20 lg:h-32" src="@/assets/images/rafikey-bot.png" alt="rafikey--bot"/>
+      <img class="" src="@/assets/images/rafikey-bot.png" alt="rafikey--bot"/>
     </div>
     <div class="flex space-x-2">
       <div class="w-8 h-3 rounded-md animate-pulse dark:bg-casablanca-400"></div>
