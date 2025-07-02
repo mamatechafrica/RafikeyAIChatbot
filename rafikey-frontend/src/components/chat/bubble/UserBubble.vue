5f1bd8fe7e40f8b51c39b3c53acb4053bd1e7f69
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-02 22:47:55 +0300

    add styling

--- rafikey-frontend/src/components/chat/bubble/UserBubble.vue
+++ rafikey-frontend/src/components/chat/bubble/UserBubble.vue
@@ -25,20 +25,29 @@ const hasText = computed(() => {
 
 <template>
   <div class="chat chat-end">
-    <div class="chat-image avatar">
-      <div class="w-10 rounded-full">
-        <img
-          alt="Tailwind CSS chat bubble component"
-          src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    <div class="flex flex-row-reverse gap-2">
+      <div class="">
+        <div class="">
+          <img
+
+            alt="user-avatar"
+            src="@/assets/images/Avatar.png" />
+        </div>
       </div>
+      <div class="flex flex-col">
+        <div class="chat-header dark:text-white flex justify-end  items-end font-semibold text-sm md:text-lg">
+          {{props.userName}}
+          <time class="text-xs md:text-sm opacity-50">{{props.createdAt}}</time>
+        </div>
+        <div class=" bg-lightBackground dark:bg-darkgray p-4 rounded-2xl">
+          <div v-if="hasText" v-html="props.userMessage" class="lg:!text-xl md:!text-lg !text-sm dark:text-white"></div>
+        </div>
+
+      </div>
+
     </div>
-    <div class="chat-header text-xs md:text-sm">
-      {{props.userName}}
-      <time class="text-xs md:text-sm opacity-50">{{props.createdAt}}</time>
-    </div>
-    <div class="chat-bubble bg-peach-200  py-4 rounded-2xl">
-      <div v-if="hasText" v-html="props.userMessage" class="lg:text-xl md:text-lg text-sm"></div>
-    </div>
+
+
 <!--    <div class="chat-footer opacity-50">Seen at 12:46</div>-->
   </div>
 </template>
