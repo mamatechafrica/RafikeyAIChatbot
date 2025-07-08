4a1100d53aa4115b8cc6232bfe4d113d2d7e77fd
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-08 22:53:35 +0300

    Add styling

--- rafikey-frontend/src/components/chat/bubble/RafikeyBubble.vue
+++ rafikey-frontend/src/components/chat/bubble/RafikeyBubble.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-
-import {computed} from "vue";
+import { computed } from 'vue'
 
 export interface RafikeyChatbot {
   chatbotName: string
@@ -11,19 +10,16 @@ export interface RafikeyChatbot {
   isError?: boolean
   createdAt?: string
   isGeneratingResponse: boolean
-
 }
-const props = withDefaults(defineProps<RafikeyChatbot>(),{
+
+const props = withDefaults(defineProps<RafikeyChatbot>(), {
   isTyping: true,
   issCopyable: false,
-  isError: false
+  isError: false,
 })
 
-const hasText = computed(()=>{
-  return (
-    (props.rafikeyChatbotMessage?.length  ?? 0) > 0 ||
-    (props.originalMessage?.length ?? 0 ) > 0
-  )
+const hasText = computed(() => {
+  return (props.rafikeyChatbotMessage?.length ?? 0) > 0 || (props.originalMessage?.length ?? 0) > 0
 })
 </script>
 
@@ -35,27 +31,29 @@ const hasText = computed(()=>{
           <img
             class="w-12 rounded-full"
             alt="Rafikey chatbot image"
-            src="@/assets/images/rafikey-avator.png">
+            src="@/assets/images/rafikey-avator.png"
+          />
         </div>
       </div>
       <div class="flex flex-col">
-        <div class="chat-header flex dark:text-white  justify-start items-end font-semibold text-sm md:text-lg">
-          {{props.chatbotName}}
-          <time class="text-xs md:text-sm  opacity-50">{{props.createdAt}}</time>
+        <div
+          class="chat-header flex dark:text-white justify-start items-end font-semibold text-sm md:text-lg"
+        >
+          {{ props.chatbotName }}
+          <time class="text-xs md:text-sm opacity-50">{{ props.createdAt }}</time>
         </div>
         <div class="py-4 rounded-2xl">
-          <div v-if="hasText" v-html="props.rafikeyChatbotMessage || props.originalMessage" class="dark:text-white"></div>
+          <div
+            v-if="hasText"
+            v-html="props.rafikeyChatbotMessage || props.originalMessage"
+            class="dark:text-white"
+          ></div>
           <span v-else class="loading loading-dots md:loading-lg loading-sm dark:text-white"></span>
-
         </div>
       </div>
-
-
     </div>
-<!--    <div class="chat-footer opacity-50">Delivered</div>-->
+    <!--    <div class="chat-footer opacity-50">Delivered</div>-->
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
