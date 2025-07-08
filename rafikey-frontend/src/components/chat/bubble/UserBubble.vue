f63658c0f7dc59351f583c1ac212002b7a262843
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-08 22:36:05 +0300

    Add styling

--- rafikey-frontend/src/components/chat/bubble/UserBubble.vue
+++ rafikey-frontend/src/components/chat/bubble/UserBubble.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import {computed, onMounted} from "vue";
+import { computed, onMounted } from 'vue'
 
-export interface UserBubbleProps  {
+export interface UserBubbleProps {
   userName: string
   userMessage: string
   createdAt: string
@@ -20,38 +20,34 @@ const props = defineProps<UserBubbleProps>()
 const hasText = computed(() => {
   return props.userMessage.length > 0
 })
-
 </script>
 
 <template>
   <div class="chat chat-end">
     <div class="flex flex-row-reverse gap-2">
-      <div class="">
-        <div class="">
-          <img
-
-            alt="user-avatar"
-            src="@/assets/images/Avatar.png" />
+      <div>
+        <div>
+          <img alt="user-avatar" src="@/assets/images/Avatar.png" />
         </div>
       </div>
       <div class="flex flex-col">
-        <div class="chat-header dark:text-white flex justify-end  items-end font-semibold text-sm md:text-lg">
-          {{props.userName}}
-          <time class="text-xs md:text-sm opacity-50">{{props.createdAt}}</time>
+        <div
+          class="chat-header dark:text-white flex justify-end items-end font-semibold text-sm md:text-lg"
+        >
+          {{ props.userName }}
+          <time class="text-xs md:text-sm opacity-50 flex ">{{ props.createdAt }}</time>
         </div>
-        <div class=" bg-lightBackground dark:bg-darkgray p-4 rounded-2xl">
-          <div v-if="hasText" v-html="props.userMessage" class="lg:!text-xl md:!text-lg !text-sm dark:text-white"></div>
+        <div class="bg-lightBackground dark:bg-darkgray p-4 rounded-2xl">
+          <div
+            v-if="hasText"
+            v-html="props.userMessage"
+            class="lg:!text-xl md:!text-lg !text-sm dark:text-white"
+          ></div>
         </div>
-
       </div>
-
     </div>
-
-
-<!--    <div class="chat-footer opacity-50">Seen at 12:46</div>-->
+    <!--    <div class="chat-footer opacity-50">Seen at 12:46</div>-->
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
