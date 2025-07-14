7e12c02ed4c8aee9b6bd2b0f68b797d66c0ea8ff
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-14 07:58:18 +0300

    add styling

--- rafikey-frontend/src/views/auth/terms&conditions/PrivacyPolicy_1.vue
+++ rafikey-frontend/src/views/auth/terms&conditions/PrivacyPolicy_1.vue
@@ -2,7 +2,7 @@
 import router from '@/router'
 import { computed } from 'vue'
 import imageLight from '../../../assets/images/rafikey-icon.png'
-import imageDark from '../../../assets/images/rafikey-icon-black.png'
+import imageDark from '../../../assets/images/rafikey-icon-dark.png'
 
 const isDark = localStorage.getItem('darkMode')
 console.log('we have mode', isDark)
@@ -14,15 +14,15 @@ const toggleImage = computed(() => {
 </script>
 
 <template>
-  <div class="h-screen w-full dark:bg-lightgray ">
-    <div class="w-10/12 mx-auto">
+  <div class="min-h-screen w-full dark:bg-lightgray">
+    <div class="md:w-10/12 w-11/12 mx-auto">
       <div class="grid grid-cols-3 sticky top-0 z-40 bg-white dark:bg-lightgray">
         <div class="col-span-1 flex flex-col justify-end">
           <div
             @click="router.go(-1)"
-            class="flex lg:pb-10 pb-8 cursor-pointer text-gray-1000 justify-start w-fit hover:bg-transparent border-none hover:border-none btn btn-sm btn-ghost shadow-none">
-            <span class="material-icons-outlined dark:text-white ">chevron_left</span>
-            <span class="lg:text-lg text-sm font-light dark:text-white">Back to Sign Up</span>
+            class="flex lg:pb-10 pb-8 cursor-pointer text-gray-1000 justify-start text-nowrap hover:bg-transparent border-none hover:border-none btn btn-sm btn-ghost shadow-none">
+            <span class="material-icons-outlined dark:text-white  md:text-lg text-sm ">chevron_left</span>
+            <span class=" md:text-lg text-sm font-light dark:text-white">Back to Sign Up</span>
           </div>
         </div>
         <div class="flex invisible lg:visible justify-center col-span-1">
@@ -35,18 +35,18 @@ const toggleImage = computed(() => {
         </div>
         <div class="flex flex-col items-center text-xl space-y-6">
           <div>
-            <p class="lg:text-xl text-lg text-black dark:text-white">We follow strict data protection rules (GDPR-compliant)</p>
+            <p class="md:text-xl text-sm text-black dark:text-white">We follow strict data protection rules (GDPR-compliant)</p>
           </div>
           <div class="flex flex-col items-center">
-            <p class="text-center lg:text-xl text-lg dark:text-white">To male sure your details stay safe and confidential.</p>
-            <p class="text-center lg:text-xl text-lg dark:text-white">At Rafikey, your privacy is a top priority. Any information you share is only used to improve your experience and provide the support you need. Wee never share your personal data with anyone without your permission.</p>
+            <p class="md:text-xl text-sm dark:text-white">To male sure your details stay safe and confidential.</p>
+            <p class="text-center md:text-xl text-sm dark:text-white">At Rafikey, your privacy is a top priority. Any information you share is only used to improve your experience and provide the support you need. Wee never share your personal data with anyone without your permission.</p>
           </div>
         </div>
         <div class="w-3/4 mx-auto" >
-          <p class="text-center lg:text-xl text-lg text-black dark:text-white">You can learn more about how we collect, use and protect your data by reading our</p>
+          <p class="text-center md:text-xl text-sm  text-black dark:text-white">You can learn more about how we collect, use and protect your data by reading our</p>
         </div>
         <div class="flex flex-col items-center">
-          <router-link to="/auth/register/privacy-policy-2" class="text-casablanca-500 border-b text-xl border-casablanca-500">Continue</router-link>
+          <router-link to="/auth/register/privacy-policy-2" class="text-casablanca-500 border-b md:text-xl text-sm border-casablanca-500">Continue</router-link>
 <!--          <div class="border-b  border-2 border-gray-500 w-1/4 mx-auto"></div>-->
         </div>
 
--- rafikey-frontend/src/views/auth/terms&conditions/PrivacyPolicy_2.vue
+++ rafikey-frontend/src/views/auth/terms&conditions/PrivacyPolicy_2.vue
@@ -2,7 +2,7 @@
 import { useRouter } from 'vue-router'
 import { computed } from 'vue'
 import imageLight from '../../../assets/images/rafikey-icon.png'
-import imageDark from '../../../assets/images/rafikey-icon-black.png'
+import imageDark from '../../../assets/images/rafikey-icon-dark.png'
 
 const router = useRouter()
 const isDark = localStorage.getItem('darkMode')
@@ -14,18 +14,18 @@ const toggleImage = computed(() => {
 
 <template>
   <div class="h-full w-full dark:bg-lightgray">
-    <div class="w-10/12 mx-auto pb-10">
+    <div class="md:w-10/12 w-11/12 mx-auto pb-10">
       <div class="grid grid-cols-3 sticky top-0 z-40 bg-white dark:bg-lightgray">
         <div class="col-span-1 flex flex-col justify-end">
           <div
             @click="router.go(-1)"
-            class="flex lg:pb-10 pb-8 cursor-pointer text-gray-1000 justify-start w-fit hover:bg-transparent border-none hover:border-none btn btn-sm btn-ghost shadow-none"
+            class="flex lg:pb-10 pb-8 cursor-pointer text-nowrap text-gray-1000 justify-start w-fit hover:bg-transparent border-none hover:border-none btn btn-sm btn-ghost shadow-none"
           >
-            <span class="material-icons-outlined dark:text-white">chevron_left</span>
-            <span class="lg:text-lg text-sm font-light dark:text-white">Back to Sign Up</span>
+            <span class="material-icons-outlined dark:text-white md:text-lg text-sm">chevron_left</span>
+            <span class="md:text-lg text-sm font-light dark:text-white">Back to Sign Up</span>
           </div>
         </div>
-        <div class="flex invisible lg:visible justify-center col-span-1">
+        <div class="flex invisible lg:visible justify-center col-span-1">lg:text-3xl text-xl
           <img :src="toggleImage" alt="rafikey-icon" />
         </div>
       </div>
@@ -34,7 +34,7 @@ const toggleImage = computed(() => {
           <div class="flex justify-end">
             <router-link
               to="/auth/register/terms-of-service"
-              class="bg-casablanca-200 lg:text-lg text-sm w-fit rounded-lg flex justify-center items-center px-2"
+              class="bg-casablanca-200 md:text-lg text-xs w-fit rounded-lg flex justify-center items-center px-2"
             >
               <span>Terms of Use</span></router-link
             >
@@ -51,63 +51,63 @@ const toggleImage = computed(() => {
 
         <div class="flex flex-col lg:text-xl text-lg space-y-4 lg:px-6 px-1 dark:text-white">
           <div class="">
-            <p>
+            <p class="md:text-xl text-sm ">
               At Rafikey, your privacy and safety comes first. This policy explains what personal
               information we collect, wky we collect it, and how we keep it safe. We are committed
               to protecting your data and following the General Data Protection Regulation (GDPR).
             </p>
           </div>
           <div class="">
-            <h2 class="lg:text-3xl text-xl font-semibold">What We Collect</h2>
-            <p>We only collect the information you choose to give us, such as:</p>
+            <h2 class="md:text-3xl text-lg font-semibold">What We Collect</h2>
+            <p class="md:text-xl text-sm ">We only collect the information you choose to give us, such as:</p>
             <ul class="list-disc pl-10">
-              <li>Username (can be anonymous)</li>
-              <li>Email address(for login and support)</li>
-              <li>Password(encrypted and never visible to anyone)</li>
-              <li>Age range, and relationship status(to personalize your experience)</li>
+              <li class="md:text-xl text-sm ">Username (can be anonymous)</li>
+              <li class="md:text-xl text-sm ">Email address(for login and support)</li>
+              <li class="md:text-xl text-sm ">Password(encrypted and never visible to anyone)</li>
+              <li class="md:text-xl text-sm ">Age range, and relationship status(to personalize your experience)</li>
             </ul>
-            <p>
+            <p class="md:text-xl text-sm ">
               We do not collect real names, phone numbers, or any sensitive data unless you choose
               to share it.
             </p>
           </div>
           <div class="">
-            <h2 class="lg:text-3xl text-xl font-semibold">Why We Collect It</h2>
-            <p>We use your information to:</p>
+            <h2 class="md:text-3xl text-lg  font-semibold">Why We Collect It</h2>
+            <p class="md:text-xl text-sm ">We use your information to:</p>
             <ul class="list-disc pl-10">
-              <li>Personalize support and content for your age and identity</li>
-              <li>Keep your account secure</li>
-              <li>Improve Rafikey to better serve your needs</li>
+              <li class="md:text-xl text-sm">Personalize support and content for your age and identity</li>
+              <li class="md:text-xl text-sm">Keep your account secure</li>
+              <li class="md:text-xl text-sm ">Improve Rafikey to better serve your needs</li>
             </ul>
-            <p>
+            <p class="md:text-xl text-sm">
               We never sell or share your information with third parties for advertising or
               tracking.
             </p>
           </div>
           <div class="">
-            <h2 class="lg:text-3xl text-xl font-semibold">How We Keep It Safe</h2>
+            <h2 class="md:text-3xl text-lg  font-semibold">How We Keep It Safe</h2>
             <ul class="list-disc pl-10">
-              <li>All passwords are securely encrypted.</li>
-              <li>Your data is stored on secure servers.</li>
-              <li>
+              <li class="md:text-xl text-sm">All passwords are securely encrypted.</li>
+              <li class="md:text-xl text-sm">Your data is stored on secure servers.</li>
+              <li class="md:text-xl text-sm">
                 Only authorized Rafikey staff can access limited data - only when needed for
                 support.
               </li>
             </ul>
-            <p>
+            <p class="md:text-xl text-sm">
               We never sell or share your information with third parties for advertising or
               tracking.
             </p>
           </div>
           <div class="">
-            <h2 class="lg:text-3xl text-xl font-semibold">Your Rights</h2>
-            <p>You have the right to:</p>
+            <h2 class="md:text-3xl text-lg  font-semibold">Your Rights</h2>
+            <p class="md:text-xl text-sm">You have the right to:</p>
             <ul class="list-disc pl-10">
-              <li>Ask what data we hold about you.</li>
-              <li>Request updates or deletion of your data</li>
-              <li>Withdraw consent at any time</li>
+              <li class="md:text-xl text-sm">Ask what data we hold about you.</li>
+              <li class="md:text-xl text-sm">Request updates or deletion of your data</li>
+              <li class="md:text-xl text-sm">Withdraw consent at any time</li>
             </ul>
-            <p>
+            <p class="md:text-xl text-sm">
               To do so, email us at
               <a href="mailto:Support@rafikey.org" class="text-casablanca-600"
                 >support@rafikey.org</a
@@ -115,22 +115,22 @@ const toggleImage = computed(() => {
             </p>
           </div>
           <div class="">
-            <h2 class="lg:text-3xl text-xl font-semibold">Cookies</h2>
-            <p>
+            <h2 class="lg:text-3xl text-lg font-semibold">Cookies</h2>
+            <p class="md:text-xl text-sm">
               We use minimal cookies to help the website function properly. We do not use tracking
               cookies or ad-based cookies.
             </p>
           </div>
           <div class="">
-            <h2 class="lg:text-3xl text-xl font-semibold">GDPR Compliance</h2>
-            <p>
+            <h2 class="lg:text-3xl text-lg font-semibold">GDPR Compliance</h2>
+            <p class="md:text-xl text-sm">
               Rafikey is fully GDPR-compliant. If you're under 16, make sure sure you have
               permission from a parent or guardian before signing up.
             </p>
           </div>
           <div class="">
-            <h2 class="lg:text-3xl text-xl font-semibold">Contact Us</h2>
-            <p>
+            <h2  class="lg:text-3xl text-lg font-semibold">Contact Us</h2>
+            <p class="md:text-xl text-sm">
               If you have questions or concerns about your privacy, contact us at
               <a href="mailto:Support@rafikey.org" class="text-casablanca-600"
                 >support@rafikey.org</a
