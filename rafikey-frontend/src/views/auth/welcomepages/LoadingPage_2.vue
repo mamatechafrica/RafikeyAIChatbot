0a3e65fe3ae4f15b03c0a282d975a896dbf82324
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-15 19:37:40 +0300

    add styling

--- rafikey-frontend/src/views/auth/welcomepages/LoadingPage_2.vue
+++ rafikey-frontend/src/views/auth/welcomepages/LoadingPage_2.vue
@@ -14,7 +14,7 @@ const toggleImage = computed(()=>{
 <template>
   <div class="h-screen w-full space-y-5 dark:bg-lightgray">
     <div class="flex w-full justify-center ">
-      <img :src="toggleImage"  alt="rafikey-logo-image"  class="invisible lg:visible" />
+      <img :src="toggleImage"  alt="rafikey-logo-image"  class="invisible md:visible" />
     </div>
     <div class="flex flex-col py-10 items-center mx-auto lg:w-8/12 xl:w-6/12  w-10/12 space-y-12 bg-lightBackground dark:bg-darkgray rounded-2xl">
       <div class="">
@@ -30,9 +30,9 @@ const toggleImage = computed(()=>{
         </p>
       </div>
       <div class="flex space-x-2">
-        <div class="w-8 h-3 rounded-md animate-pulse dark:bg-casablanca-400"></div>
-        <div class="w-3 h-3 rounded-full animate-pulse dark:bg-casablanca-200"></div>
-        <div class="w-3 h-3 rounded-full animate-pulse dark:bg-casablanca-300"></div>
+        <div class="w-8 h-3 rounded-md animate-pulse bg-casablanca-400"></div>
+        <div class="w-3 h-3 rounded-full animate-pulse bg-casablanca-200"></div>
+        <div class="w-3 h-3 rounded-full animate-pulse bg-casablanca-300"></div>
       </div>
     </div>
   </div>
