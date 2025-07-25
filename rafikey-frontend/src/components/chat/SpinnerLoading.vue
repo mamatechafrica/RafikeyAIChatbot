b5e745e63379f7d16bb70e0de6001c8fb1b0ea72
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-25 16:05:47 +0300

    add SpinnerLoading component

---
+++ rafikey-frontend/src/components/chat/SpinnerLoading.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="h-screen flex items-center justify-center">
+  <div>
+    <span class="loading loading-spinner loading-lg md:text-xl text-lg dark:text-lightBackground text-darkgray"></span>
+  </div>
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
