1ed987e86f140df2f2e5b3b08f9ec8792deb512b
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-28 11:32:04 +0300

    add styling

--- rafikey-frontend/src/views/auth/signup/GetToKnowYou.vue
+++ rafikey-frontend/src/views/auth/signup/GetToKnowYou.vue
@@ -161,7 +161,6 @@ const createAccountHandler = () =>{
       })
   } else {
     // show an error message
-    console.error('Please fill all the fields')
     signupError.isError= true
     signupError.message =  'Please fill in all fields correctly'
   }
@@ -182,20 +181,20 @@ onBeforeMount(()=>{
 
 <template>
 
-    <div class="h-screen w-full hidden md:flex items-center justify-center dark:bg-lightgray">
+    <div class="h-screen w-full overflow-hidden hidden md:flex items-center justify-center dark:bg-lightgray">
       <div class="bg-lightBackground dark:bg-darkgray flex flex-col w-10/12 mx-auto rounded-2xl pb-10">
         <div>
           <div>
             <img :src="toggleImage" alt="rafikey-logo" />
           </div>
-          <div class="w-10/12 rounded-2xl   mx-auto space-y-8">
+          <div class="w-10/12 rounded-2xl   mx-auto" :class="[signupError.isError? ' space-y-4': 'space-y-6']">
             <div class="flex flex-col items-center">
-              <h2 class="text-4xl  font-semibold dark:text-white">Let's Get To Know You Better</h2>
+              <h2 class="text-4xl font-semibold dark:text-white">Let's Get To Know You Better</h2>
             </div>
             <div class="border-b border-gray-400 w-1/2 xl:w-1/4 mx-auto dark:border-stone-400"></div>
             <div class="space-y-4">
               <div class="flex flex-col items-center">
-                <p class="text-xl dark:text-white">How old are you</p>
+                <p class="text-lg dark:text-white">How old are you</p>
                 <span class="text-lg text-gray-700 dark:text-stone-300">So we can tailor content and support</span>
               </div>
               <div class="">
@@ -205,7 +204,7 @@ onBeforeMount(()=>{
 
             <div class="space-y-4">
               <div class="flex justify-center">
-                <p class="text-xl text-white">Which gender are you</p>
+                <p class="text-lg text-white">Which gender are you</p>
               </div>
               <div class="">
                 <RadioGroup :radio-type="genderSet" @change="selectedRadio"/>
@@ -213,7 +212,7 @@ onBeforeMount(()=>{
             </div>
             <div class="space-y-4">
               <div class="flex justify-center flex-col items-center">
-                <p class="text-xl text-white">Are you in a relationship?</p>
+                <p class="text-lg text-white">Are you in a relationship?</p>
                 <span class="text-lg text-gray-700 dark:text-stone-300">Helps with personalized advice</span>
               </div>
 
@@ -221,27 +220,27 @@ onBeforeMount(()=>{
                 <RadioGroup :radio-type="relationshipStatus" @change="selectedRadio"/>
               </div>
             </div>
-            <div v-if="signupError.isError" class="flex gap-2">
+            <div v-if="signupError.isError" class="flex gap-2 justify-start w-full lg:w-3/4  xl:w-1/2 mx-auto ">
               <span class="material-icons-outlined text-rose-500">error</span>
               <span class="text-rose-500">{{signupError.message}}</span>
             </div>
             <div class="border-b border-gray-400 w-1/2 xl:w-1/4  mx-auto dark:border-stone-400"></div>
             <div class="flex flex-col">
               <div class="flex gap-1 xl:gap-1 justify-center">
-                <input type="checkbox" v-model="getToKnowYouData.isTermsCondition" class="checkbox text-casablanca-400 border-slate-800 dark:border-white" />
-                <p class="text-center dark:text-white">
+                <input type="checkbox" v-model="getToKnowYouData.isTermsCondition" class="checkbox text-button-light border-slate-800 dark:border-white" />
+                <p class="text-lg text-center dark:text-white">
                   We follow strict privacy rules (GDPR-compliant). Your info is safe and won't be shared
                   without your consent.
                 </p>
               </div>
 
               <div class="flex justify-center">
-                <span class="pe-1 dark:text-white">Read our full</span>
-                <router-link to="/auth/register/privacy-policy-1" class="cursor-pointer text-casablanca-600"
+                <span class="pe-1 dark:text-white text-lg">Read our full</span>
+                <router-link to="/auth/register/privacy-policy-1" class="text-lg cursor-pointer text-button-light"
                 >Terms of Use
                 </router-link>
-                <span class="px-1 dark:text-white">and</span>
-                <router-link to="/auth/register/privacy-policy-1" class="cursor-pointer text-casablanca-600">
+                <span class="px-1 dark:text-white text-lg">and</span>
+                <router-link to="/auth/register/privacy-policy-1" class="text-lg cursor-pointer text-button-light">
                   Privacy Policy
                 </router-link>
               </div>
@@ -250,9 +249,7 @@ onBeforeMount(()=>{
             <div class="flex justify-center">
               <button
                 @click="createAccountHandler"
-
-                :class="[!everyThingOk ? 'bg-casablanca-100' : 'bg-casablanca-300 hover:bg-casablanca-400']"
-                class="btn w-full xl:w-1/2  btn-sm text-lg rounded-2xl py-6"
+                class="btn w-full xl:w-1/2  btn-sm text-lg rounded-2xl py-6 bg-button-light border-none shadow-none"
               >
                 <span v-if="!isLoading" class="">Sign up</span>
                 <span v-else class="loading loading-spinner loading-sm"></span>
@@ -269,11 +266,11 @@ onBeforeMount(()=>{
 
 
   <!--    Small screen -->
-  <div class="md:hidden block min-h-screen w-full dark:bg-darkgray">
+  <div class="md:hidden block min-h-screen w-full overflow-hidden dark:bg-darkgray">
     <div class="flex justify-center">
       <img src="@/assets/images/rafikey-key.png" alt="rafikey-logo" class="w-24" />
     </div>
-    <div class="md:px-20 px-6 md:space-y-6 space-y-2">
+    <div class="md:px-20 px-6 space-y-2  sm:space-y-4">
       <div>
         <h2 class="md:text-2xl text-lg font-semibold text-center dark:text-white">Let's Know You Better</h2>
       </div>
@@ -310,26 +307,24 @@ onBeforeMount(()=>{
         </p>
       </div>
       <div class="">
-        <input type="checkbox" v-model="getToKnowYouData.isTermsCondition" class="checkbox text-casablanca-400 border-slate-800 dark:border-white" />
+        <input type="checkbox" v-model="getToKnowYouData.isTermsCondition" class="checkbox text-button-light border-slate-800 dark:border-white" />
         <span class="text-gray-950 dark:text-white md:text-lg text-sm ps-4"
           >Creating an account means you are okay with our</span
         >
-        <router-link to="/auth/register/privacy-policy-1" class="cursor-pointer md:text-lg text-sm text-casablanca-600">
+        <router-link to="/auth/register/privacy-policy-1" class="cursor-pointer md:text-lg text-sm text-button-light">
           Terms of Use
         </router-link>
         <span class="dark:text-white md:text-lg text-sm"> and</span>
-        <router-link to="/auth/register/privacy-policy-1" class="md:text-lg text-sm cursor-pointer text-casablanca-600">
+        <router-link to="/auth/register/privacy-policy-1" class="md:text-lg text-sm cursor-pointer text-button-light">
           Privacy Policy
         </router-link>
       </div>
       <div class="w-3/4 mx-auto flex justify-between items-center">
         <button
           @click="createAccountHandler"
-
-          :class="[!everyThingOk ? 'bg-casablanca-100' : 'bg-casablanca-300 hover:bg-casablanca-400']"
-          class="btn w-full btn-sm 0 text-lg rounded-2xl py-5"
+          class="btn w-full btn-sm 0 text-lg rounded-2xl py-5 bg-button-light border-none shadow-none"
         >
-          <span v-if="!isLoading">Sign up</span>
+          <span v-if="!isLoading" class="text-sm">Sign up</span>
           <span v-else class="loading loading-spinner loading-sm"></span>
         </button>
       </div>
