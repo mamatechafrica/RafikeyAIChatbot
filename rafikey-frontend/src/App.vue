86d939ac0a5177ebd57e0af02b6301c580834903
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-28 10:37:51 +0300

    add notification Toaster

--- rafikey-frontend/src/App.vue
+++ rafikey-frontend/src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import  {onMounted} from "vue";
 
+import { useNotificationStore } from '@/stores'
+import ToastContainer from '@/components/toasts/ToastContainer.vue'
+import ToastAlert from '@/components/toasts/ToastAlert.vue'
+
+const notificationStore = useNotificationStore()
+
 onMounted(() => {
   document.documentElement.setAttribute('data-theme', 'light')
 })
@@ -15,7 +21,19 @@ onMounted(() => {
     </RouterView>
 
 
-
+  <teleport to="body">
+    <ToastContainer v-if="notificationStore.hasNotifications">
+      <template v-for="notification in notificationStore.getNotifications" :key="notification.id">
+        <ToastAlert
+          v-if="notification.id && notification.isShown"
+          :id="notification.id"
+          :is-shown="notification.isShown"
+          :message="notification.message"
+          :type="notification.type"
+        />
+      </template>
+    </ToastContainer>
+  </teleport>
 </template>
 
 <style scoped>
