272694cbbb555616cd36a50f9f1dde5bc1362747
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-30 21:56:54 +0300

    pass props to ui

--- rafikey-frontend/src/components/chat/ErrorScreen.vue
+++ rafikey-frontend/src/components/chat/ErrorScreen.vue
@@ -11,6 +11,8 @@ const props = defineProps<{
   errorMessage?: string,
   isLoggedIn?: boolean,
 }>()
+
+
 const refreshPage = () => {
 window.location.reload()
 }
@@ -45,7 +47,7 @@ const regenerateResponse = ()=>{
 </script>
 
 <template>
-  <div class="">
+  <div class="pt-4">
     <div class="border border-rose-500 bg-rose-200 rounded-xl">
       <div class="p-4 space-y-3">
         <div class="flex items-center gap-2">
@@ -53,24 +55,33 @@ const regenerateResponse = ()=>{
             <span class="material-icons-outlined text-rose-400">info</span>
           </div>
           <div>
-            <span class="lg:text-lg text-sm">Network Issue</span>
+            <span class="lg:text-lg text-sm">Opps!</span>
           </div>
         </div>
         <div class="flex justify-between">
-          <p class="lg:text-lg text-sm">Hmm... it looks like you're offline. Check your network connection and refresh to continue</p>
-          <button class="btn btn-sm bg-black shadow-none  rounded-xl p-6 " v-on:click="refreshPage">
-            <span class="text-white lg:text-lg text-sm">Refresh chat</span>
+          <p class="lg:text-lg text-sm">{{props.errorMessage}}</p>
+          <button
+            @click="refreshChat"
+            v-if="props.isLoggedIn" class="btn btn-sm bg-black shadow-none  rounded-xl p-6 " v-on:click="refreshPage">
+            <span  class="text-white lg:text-lg text-sm">Refresh chat</span>
           </button>
+          <button
+            @click="loginHandler"
+            v-else class="btn btn-sm bg-black shadow-none  rounded-xl md:p-6 p-4 " v-on:click="refreshPage">
+            <span  class="text-white lg:text-lg text-sm">Login</span>
+
+          </button>
+
         </div>
       </div>
 
     </div>
 
-    <div class="fixed bottom-32  left-1/2 -translate-x-1/2 -translate-y-1/2 z-50">
+    <div class="fixed bottom-0  left-1/2 -translate-x-1/2 -translate-y-1/2 z-50" v-if="props.isLoggedIn">
 
         <button @click = 'regenerateResponse' class="bg-casablanca-300 p-4 rounded-xl flex">
           <span class="material-icons-outlined lg:text-lg text-sm">loop</span>
-          <span class="lg:text-lg text-sm">Regenerate Response</span>
+          <span class="lg:text-lg text-sm text-nowrap">Regenerate Response</span>
         </button>
 
 
