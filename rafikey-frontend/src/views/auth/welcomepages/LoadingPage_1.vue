ad4cf8682e1d6b868f9d7d5e8ee49195d6b7582b
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-08-04 15:23:59 +0300

    remove toggleDark feature

--- rafikey-frontend/src/views/auth/welcomepages/LoadingPage_1.vue
+++ rafikey-frontend/src/views/auth/welcomepages/LoadingPage_1.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import imageLight from '@/assets/images/rafikey-icon-light.png'
-import imageDark from '@/assets/images/rafikey-icon-dark.png'
-import { computed  } from 'vue'
+// import imageLight from '@/assets/images/rafikey-icon-light.png'
+// import imageDark from '@/assets/images/rafikey-icon-dark.png'
+// import { computed  } from 'vue'
 import { useRafikeyChatbotStore } from '@/stores'
 
 
 const chatbotStore = useRafikeyChatbotStore()
-const toggleImage = computed(()=>{
-  return chatbotStore.isDarkMode?  imageDark: imageLight
-})
+// const toggleImage = computed(()=>{
+//   return chatbotStore.isDarkMode? imageLight: imageDark
+// })
 </script>
 
 <template>
   <div class="h-screen w-full ">
-    <div class="flex justify-center items-center h-full dark:bg-darkgray ">
-      <img :src="toggleImage" alt="rafikey-icon" class="w-40 lg:w-60"/>
+    <div class="flex justify-center items-center h-full  ">
+      <img src='@/assets/images/rafikey-icon-light.png' alt="rafikey-icon" class="w-40 lg:w-60"/>
     </div>
   </div>
 </template>
