304d28cf4cd1ce02d16e4af5ed00822e62b390b6
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-08-04 15:16:46 +0300

    add styling

--- rafikey-frontend/src/views/auth/UserForgotPassword.vue
+++ rafikey-frontend/src/views/auth/UserForgotPassword.vue
@@ -44,7 +44,9 @@ const {
   meta: emailMeta,
 } = useField('email', emailValidator)
 
-watch(()=> emailData.value, value => {
+watch(
+  () => emailData.value,
+  (value) => {
     if (value) {
       email.value = value
     }
@@ -52,172 +54,193 @@ watch(()=> emailData.value, value => {
 )
 
 // before going to know you page, check if all fields are valid
-const everyThingOk = computed(()=>{
+const everyThingOk = computed(() => {
   return emailMeta.validated && emailMeta.valid
 })
 
-watch(everyThingOk, (value) =>{
-  if(value){
+watch(everyThingOk, (value) => {
+  if (value) {
     forgotPasswordError.isError = false
     forgotPasswordError.message = ''
   }
 })
 
-
-const forgotPasswordHandler = () =>{
-  if(everyThingOk.value){
+const forgotPasswordHandler = () => {
+  if (everyThingOk.value) {
     isLoading.value = true
-    authStore.forgotPassword(email.value)
-      .then(response =>{
-        if(response?.result === 'ok'){
+    authStore
+      .forgotPassword(email.value)
+      .then((response) => {
+        if (response?.result === 'ok') {
           showSweetAlert({
-           type: 'success',
-            message: response.message
+            type: 'success',
+            message: response.message,
           })
-        } else{
+        } else {
           showSweetAlert({
             type: 'error',
-            message: 'An error occurred please try again later'
+            message: 'An error occurred please try again later',
           })
         }
       })
-      .catch(()=>{
+      .catch(() => {
         showSweetAlert({
           type: 'error',
-          message: 'An error occurred please try again later'
+          message: 'An error occurred please try again later',
         })
       })
-      .finally(()=>{
+      .finally(() => {
         isLoading.value = false
       })
-  } else{
+  } else {
     forgotPasswordError.isError = true
     forgotPasswordError.message = 'Please fill in your email'
   }
 }
-
-
-
-
 </script>
 
 <template>
-  <div>
-    <div class="h-screen w-full overflow-hidden hidden lg:block dark:bg-lightgray">
-      <div class="w-10/12 grid grid-cols-2 mx-auto">
-        <!--      left-side-->
-        <div class="col-span-1">
+  <div class="h-screen w-full py-8 overflow-hidden hidden lg:block
+">
+    <div class="w-10/12 grid grid-cols-2 mx-auto gap-8">
+      <!--      left-side-->
+      <div class="col-span-1">
+        <div>
+          <img src="@/assets/images/rafikey-icon-light.png" alt="rafikey-icon" class="w-60" />
+        </div>
+        <div class="flex flex-col space-y-12">
+          <div
+            @click="router.go(-1)"
+            class="flex cursor-pointer text-gray-1000 justify-start w-fit hover:bg-transparent border-none hover:border-none btn btn-sm btn-ghost shadow-none"
+          >
+            <span class="material-icons-outlined ">chevron_left</span>
+            <span class="text-extra-small font-light ">Back to login</span>
+          </div>
+          <div class="flex flex-col space-y-6">
+            <h2 class="text-extra-extra-large font-semibold ">
+              Forgot your password?
+            </h2>
+            <p class="text-large text-gray-700 ">
+              Don't worry, happens to all of us. Enter your email bellow to recover your password
+            </p>
+          </div>
+          <!--          forgot password form-->
           <div>
-            <img :src="toggleImage " alt="rafikey-icon" />
-          </div>
-          <div class="flex flex-col space-y-12">
-            <div
-              @click="router.go(-1)"
-              class="flex  cursor-pointer text-gray-1000 justify-start w-fit hover:bg-transparent border-none hover:border-none btn btn-sm btn-ghost shadow-none">
-              <span class="material-icons-outlined dark:text-white">chevron_left</span>
-              <span class="text-lg font-light dark:text-white">Back to login</span>
-            </div>
-            <div class="flex flex-col space-y-6">
-              <h2 class="lg:text-3xl text-xl font-semibold dark:text-white">Forgot your password?</h2>
-              <p class="lg:text-xl text-lg text-gray-700 dark:text-stone-300">Don't worry, happens to all of us. Enter
-                your email bellow to recover your password</p>
-            </div>
-            <!--          forgot password form-->
-            <div>
-              <form class="flex flex-col w-10/12" :class="forgotPasswordError.isError?' space-y-4 xl:space-y-12': 'space-y-12 '">
-                <div class="relative">
-                  <input
-                    v-model="emailData"
-                    id="email"
-                    type="email"
-                    required
-                    class="w-full border-2 border-gray-500 rounded-2xl p-4 lg:text-lg text-sm dark:bg-lightgray dark:text-stone-400"
-                    placeholder="johndoe@gmail.com"
-                  />
-                  <label
-                    class="absolute left-1 -top-3 text-slate-600 px-1 lg:text-lg text-sm bg-white dark:bg-lightgray dark:text-white"
-                    for="email">
-                    Email address
-                  </label>
-                  <span v-if="emailMeta.validated && !emailMeta.valid"  class="text-rose-500">{{emailErrorMessage}}</span>
-
-                </div>
-                <div v-if="forgotPasswordError.isError" class="flex gap-2">
-                  <span class="material-icons-outlined text-rose-500">error</span>
-                  <span class="text-rose-500">{{forgotPasswordError.message}}</span>
-                </div>
-                <div
-                  @click="forgotPasswordHandler"
-                  class="btn btn-sm  text-lg rounded-2xl py-6 bg-button-light border-none shadow-none">
-                  <span v-if="!isLoading">Submit</span>
-                  <span v-else class="loading loading-spinner loading-sm"></span>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-        <!--      right--side-->
-        <div class="col-span-1 flex items-center justify-center">
-          <div class="bg-gray-100 rounded-2xl dark:bg-transparent">
-            <img src="@/assets/images/forgot-password.png" alt="forgot-password-image" />
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-    <!--    Small screens-->
-    <div class="lg:hidden block overflow-hidden h-screen w-full dark:bg-lightgray">
-
-      <div class="flex justify-center">
-        <img src="@/assets/images/rafikey-key.png" alt="rafikey-logo" class="w-24"/>
-      </div>
-      <div class="flex flex-col md:px-20 px-10 space-y-6">
-        <div class="flex flex-col items-center space-y-4">
-          <h2 class="md:text-2xl text-lg font-semibold dark:text-white">Forgot Password</h2>
-          <div class="md:space-y-2 space-y-0.5">
-            <p class="md:text-xl sm:text-lg text-gray-700 text-center dark:text-stone-300">Don't worry. Happens to all of us. </p>
-            <p class="md:text-xl sm:text-lg text-gray-700 text-center dark:text-stone-300">Enter your Email below to recover your password</p>
-          </div>
-        </div>
-        <div class="border-b border-gray-400 w-full"></div>
-        <form class="space-y-8">
-
-          <div class="space-y-3">
-            <label for="email" class="text-black md:text-lg text-sm  dark:text-white">Email</label>
-            <input
+            <form
+              class="flex flex-col w-10/12"
+              :class="forgotPasswordError.isError ? ' space-y-4 xl:space-y-12' : 'space-y-12 '"
+            >
+              <div class="relative">
+                <input
                   v-model="emailData"
                   id="email"
-                   type="email"
-                   class=" w-full border-2 border-gray-500 rounded-2xl md:p-4 p-3  md:text-lg text-sm dark:bg-lightgray dark:text-stone-400"
-                   required
-                   placeholder="Enter your email"
-            />
-            <span v-if="emailMeta.validated && !emailMeta.valid"  class="text-rose-500">{{emailErrorMessage}}</span>
+                  type="email"
+                  required
+                  class="w-full border-[1px] border-gray-300 rounded-2xl p-4 text-small "
+                  placeholder="johndoe@gmail.com"
+                />
+                <label
+                  class="absolute left-1 -top-3 text-slate-600 px-1 text-text-extra-small bg-white "
+                  for="email"
+                >
+                  Email address
+                </label>
+                <span v-if="emailMeta.validated && !emailMeta.valid" class="text-rose-500">{{
+                  emailErrorMessage
+                }}</span>
+              </div>
+              <div v-if="forgotPasswordError.isError" class="flex gap-2">
+                <span class="material-icons-outlined text-rose-500">error</span>
+                <span class="text-rose-500">{{ forgotPasswordError.message }}</span>
+              </div>
+              <div
+                @click="forgotPasswordHandler"
+                class="btn btn-sm rounded-2xl py-6 bg-button-light border-none shadow-none"
+              >
+                <span v-if="!isLoading" class="text-extra-small">Submit</span>
+                <span v-else class="loading loading-spinner loading-sm"></span>
+              </div>
+            </form>
           </div>
-        </form>
-        <div v-if="forgotPasswordError.isError" class="flex gap-2">
-          <span class="material-icons-outlined text-rose-500">error</span>
-          <span class="text-rose-500">{{forgotPasswordError.message}}</span>
         </div>
-        <div class="border-b border-gray-400 w-full pt-6"></div>
-
-        <div class="w-3/4 mx-auto flex justify-between items-center pt-6">
-          <button
-            @click="forgotPasswordHandler"
-            class="btn w-full btn-sm  md:text-lg text-sm rounded-2xl py-6 bg-button-light border-none shadow-none">
-            <span v-if="!isLoading" class="text-sm md:text-lg">Submit</span>
-            <span v-else class="loading loading-spinner loading-sm"></span>
-          </button>
+      </div>
+      <!--      right--side-->
+      <div class="col-span-1 flex items-center justify-center">
+        <div class="bg-gray-100 rounded-2xl  h-5/6">
+          <img src="@/assets/images/forgot-password.png" alt="forgot-password-image" />
         </div>
-
-
       </div>
     </div>
   </div>
 
+  <!--    Small screens-->
+  <div class="lg:hidden block overflow-hidden h-screen w-full ">
+    <div class="grid grid-cols-3 gap-4 items-center px-4 py-2">
+      <div
+        @click="router.go(-1)"
+        class="col-span-1 flex cursor-pointer text-gray-1000 justify-start w-fit hover:bg-transparent border-none hover:border-none btn btn-sm btn-ghost shadow-none"
+      >
+        <span class="material-icons-outlined ">chevron_left</span>
+        <span class="text-extra-small font-light ">Back</span>
+      </div>
+
+      <div class="col-span-1flex justify-center">
+        <img src="@/assets/images/rafikey-key.png" alt="rafikey-logo" class="w-40" />
+      </div>
+    </div>
+
+    <div class="flex flex-col md:px-20 px-10 space-y-6">
+      <div class="flex flex-col items-center space-y-4">
+        <h2 class="text-extra-large font-semibold">Forgot Password</h2>
+        <div class="text-extra-small md:space-y-2 space-y-0.5">
+          <p class=" text-gray-700 text-center ">
+            Don't worry. Happens to all of us.
+          </p>
+          <p class="text-gray-700 text-center ">
+            Enter your Email below to recover your password
+          </p>
+        </div>
+      </div>
+      <div class="border-b border-gray-400 w-full"></div>
+      <form class="space-y-8">
+        <div class="space-y-3">
+          <label for="email" class="text-black text-extra-small ">Email</label>
+          <input
+            v-model="emailData"
+            id="email"
+            type="email"
+            class="w-full  border-[1px] border-gray-300  rounded-2xl  md:p-4 p-3 text-extra-small "
+            required
+            placeholder="maryjane@gmail.com"
+          />
+          <span v-if="emailMeta.validated && !emailMeta.valid" class="text-rose-500">{{
+            emailErrorMessage
+          }}</span>
+        </div>
+      </form>
+      <div v-if="forgotPasswordError.isError" class="flex gap-2">
+        <span class="material-icons-outlined text-rose-500">error</span>
+        <span class="text-rose-500">{{ forgotPasswordError.message }}</span>
+      </div>
+      <div class="border-b border-gray-400 w-full pt-6"></div>
+
+      <div class="w-3/4 mx-auto flex-col space-y-2 justify-between items-center pt-6">
+        <button
+          @click="forgotPasswordHandler"
+          class="btn w-full btn-sm md:text-lg text-sm rounded-2xl py-6 bg-button-light border-none shadow-none"
+        >
+          <span v-if="!isLoading" class="text-extra-small">Submit</span>
+          <span v-else class="loading loading-spinner loading-sm"></span>
+        </button>
+<!--        <p class="text-center text-slate-800 dark:text-white text-extra-small">-->
+<!--          Don't have an account?-->
+<!--          <router-link to="/auth/register" class="text-rose-400 text-extra-small">-->
+<!--            Sign Up-->
+<!--          </router-link>-->
+<!--        </p>-->
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
