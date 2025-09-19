96452231c1e115b4d8041d17fbb62dcddc9ece18
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-09-19 19:46:43 +0300

    add AuthLoadingButtons component

---
+++ rafikey-frontend/src/components/chat/AuthLoadingButtons.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex space-x-2">
+    <div class="w-8 h-3 rounded-md animate-pulse bg-casablanca-300"></div>
+    <div class="w-3 h-3 rounded-full animate-pulse bg-casablanca-300 opacity-50"></div>
+    <div class="w-3 h-3 rounded-full animate-pulse bg-casablanca-300 opacity-30"></div>
+  </div>
+</template>
+
+<style scoped></style>
