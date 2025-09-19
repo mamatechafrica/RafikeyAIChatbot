e82da8665d70a8ba8443c97dd8c906437ac0d1b8
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-09-19 19:55:54 +0300

    add dynamic image file path src attribute

--- rafikey-frontend/src/views/auth/UserForgotPassword.vue
+++ rafikey-frontend/src/views/auth/UserForgotPassword.vue
@@ -102,27 +102,26 @@ const forgotPasswordHandler = () => {
 </script>
 
 <template>
-  <div class="h-screen w-full py-8 overflow-hidden hidden lg:block
-">
+  <div class="h-screen w-full py-8 overflow-hidden hidden lg:block dark:bg-lightgray">
     <div class="w-10/12 grid grid-cols-2 mx-auto gap-8">
       <!--      left-side-->
       <div class="col-span-1">
         <div>
-          <img src="@/assets/images/rafikey-icon-light.png" alt="rafikey-icon" class="w-60" />
+          <img :src="toggleImage()" alt="rafikey-icon" class="w-60" />
         </div>
         <div class="flex flex-col space-y-12">
           <div
             @click="router.go(-1)"
             class="flex cursor-pointer text-gray-1000 justify-start w-fit hover:bg-transparent border-none hover:border-none btn btn-sm btn-ghost shadow-none"
           >
-            <span class="material-icons-outlined ">chevron_left</span>
-            <span class="text-extra-small font-light ">Back to login</span>
+            <span class="material-icons-outlined dark:text-white ">chevron_left</span>
+            <span class="text-extra-small font-light dark:text-white ">Back to login</span>
           </div>
           <div class="flex flex-col space-y-6">
-            <h2 class="text-extra-extra-large font-semibold ">
+            <h2 class="text-extra-extra-large font-semibold dark:text-white ">
               Forgot your password?
             </h2>
-            <p class="text-large text-gray-700 ">
+            <p class="text-large text-gray-700  dark:text-white  ">
               Don't worry, happens to all of us. Enter your email bellow to recover your password
             </p>
           </div>
@@ -138,11 +137,11 @@ const forgotPasswordHandler = () => {
                   id="email"
                   type="email"
                   required
-                  class="w-full border-[1px] border-gray-300 rounded-2xl p-4 text-small "
+                  class="w-full dark:bg-darkgray dark:text-white border-[1px] border-gray-300 rounded-2xl p-4 text-small "
                   placeholder="johndoe@gmail.com"
                 />
                 <label
-                  class="absolute left-1 -top-3 text-slate-600 px-1 text-text-extra-small bg-white "
+                  class="absolute left-1 -top-3 dark:text-white dark:bg-darkgray text-slate-600 px-1 text-text-extra-small bg-white "
                   for="email"
                 >
                   Email address
@@ -168,7 +167,7 @@ const forgotPasswordHandler = () => {
       </div>
       <!--      right--side-->
       <div class="col-span-1 flex items-center justify-center">
-        <div class="bg-gray-100 rounded-2xl  h-5/6">
+        <div class="bg-gray-100 dark:bg-lightgray rounded-2xl  h-5/6">
           <img src="@/assets/images/forgot-password.png" alt="forgot-password-image" />
         </div>
       </div>
@@ -176,29 +175,31 @@ const forgotPasswordHandler = () => {
   </div>
 
   <!--    Small screens-->
-  <div class="lg:hidden block overflow-hidden h-screen w-full ">
+  <div class="lg:hidden block overflow-hidden h-screen w-full dark:bg-lightgray">
     <div class="grid grid-cols-3 gap-4 items-center px-4 py-2">
       <div
         @click="router.go(-1)"
         class="col-span-1 flex cursor-pointer text-gray-1000 justify-start w-fit hover:bg-transparent border-none hover:border-none btn btn-sm btn-ghost shadow-none"
       >
-        <span class="material-icons-outlined ">chevron_left</span>
-        <span class="text-extra-small font-light ">Back</span>
+        <span class="material-icons-outlined dark:text-white ">chevron_left</span>
+        <span class="text-extra-small font-light dark:text-white  ">Back</span>
       </div>
 
-      <div class="col-span-1flex justify-center">
-        <img src="@/assets/images/rafikey-key.png" alt="rafikey-logo" class="w-40" />
+      <div class="col-span-1 flex justify-center">
+        <div class="w-54   flex justify-center ">
+          <img src="@/assets/images/rafikey-key.png" alt="rafikey-logo"  />
+        </div>
       </div>
     </div>
 
     <div class="flex flex-col md:px-20 px-10 space-y-6">
       <div class="flex flex-col items-center space-y-4">
-        <h2 class="text-extra-large font-semibold">Forgot Password</h2>
+        <h2 class="text-extra-large font-semibold dark:text-white ">Forgot Password</h2>
         <div class="text-extra-small md:space-y-2 space-y-0.5">
-          <p class=" text-gray-700 text-center ">
+          <p class=" text-gray-700 text-center dark:text-white ">
             Don't worry. Happens to all of us.
           </p>
-          <p class="text-gray-700 text-center ">
+          <p class="text-gray-700 text-center dark:text-white ">
             Enter your Email below to recover your password
           </p>
         </div>
@@ -206,12 +207,12 @@ const forgotPasswordHandler = () => {
       <div class="border-b border-gray-400 w-full"></div>
       <form class="space-y-8">
         <div class="space-y-3">
-          <label for="email" class="text-black text-extra-small ">Email</label>
+          <label for="email" class="dark:text-white text-black text-extra-small ">Email</label>
           <input
             v-model="emailData"
             id="email"
             type="email"
-            class="w-full  border-[1px] border-gray-300  rounded-2xl  md:p-4 p-3 text-extra-small "
+            class="w-full dark:bg-darkgray dark:text-white   border-[1px] border-gray-300  rounded-2xl  md:p-4 p-3 text-extra-small "
             required
             placeholder="maryjane@gmail.com"
           />
