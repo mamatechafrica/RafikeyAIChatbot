2d33ce3d1b22f0dbd9a51903b322689f75b43828
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-09-19 19:52:36 +0300

    add dynamic image file path src attribute

--- rafikey-frontend/src/views/auth/welcomepages/LoadingPage_1.vue
+++ rafikey-frontend/src/views/auth/welcomepages/LoadingPage_1.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-// import imageLight from '@/assets/images/rafikey-icon-light.png'
-// import imageDark from '@/assets/images/rafikey-icon-dark.png'
-// import { computed  } from 'vue'
-import { useRafikeyChatbotStore } from '@/stores'
 
-
-const chatbotStore = useRafikeyChatbotStore()
-// const toggleImage = computed(()=>{
-//   return chatbotStore.isDarkMode? imageLight: imageDark
-// })
+import { toggleImage } from '@/modules/imageToggle.ts'
 </script>
-
 <template>
-  <div class="h-screen w-full ">
+  <div class="h-screen w-full dark:bg-lightgray ">
     <div class="flex justify-center items-center h-full  ">
-      <img src='@/assets/images/rafikey-icon-light.png' alt="rafikey-icon" class="w-40 lg:w-60"/>
+      <img :src="toggleImage()" alt="rafikey-icon" class="w-40 lg:w-60"/>
     </div>
   </div>
 </template>
--- rafikey-frontend/src/views/auth/welcomepages/LoadingPage_2.vue
+++ rafikey-frontend/src/views/auth/welcomepages/LoadingPage_2.vue
@@ -1,40 +1,49 @@
 <script setup lang="ts">
-import imageLight from '@/assets/images/rafikey-icon-light.png'
-import imageDark from '@/assets/images/rafikey-icon-dark.png'
-import { computed  } from 'vue'
+import { toggleImage } from '@/modules/imageToggle.ts'
+import AuthLoadingButtons from '@/components/chat/AuthLoadingButtons.vue'
 import { useRafikeyChatbotStore } from '@/stores'
 
-// const chatbotStore = useRafikeyChatbotStore()
-// const toggleImage = computed(()=>{
-//   console.log(chatbotStore.isDarkMode)
-//   return chatbotStore.isDarkMode? imageDark : imageLight
-// })
+const chatbotSstore = useRafikeyChatbotStore()
+const moveNext = () => {
+  chatbotSstore.isMoveNext = true
+  // navigate to the next welcome page
+
+}
 </script>
 
 <template>
-  <div class="h-screen flex  justify-center items-center flex-col w-full space-y-5">
+  <div
+    class="relative h-screen flex justify-center md:block s flex-col w-full space-y-5 dark:bg-lightgray"
+  >
     <div class="flex w-full justify-center ">
-      <img src='@/assets/images/rafikey-icon-light.png' alt="rafikey-logo-image"  class="hidden md:block" />
+      <img :src="toggleImage()"  alt="rafikey-logo-image" class="hidden md:block" />
     </div>
-    <div class="flex flex-col justify-center py-10 items-center mx-auto lg:w-8/12 xl:w-6/12  w-10/12 space-y-10 bg-lightBackground rounded-2xl">
-      <div class="">
-        <p class="text-extra-extra-large-1   text-center font-semibold ">Welcome to Rafikey!</p>
+    <div class="relative w-11/12 mx-auto ">
+      <div
+        class="flex flex-col dark:bg-darkgray pt-24 pb-10 items-center mx-auto w-[calc(100%-3.75rem)] lg:w-[calc(100%-5rem)] col-span-2  space-y-10 bg-lightBackground rounded-2xl"
+      >
+        <div class="">
+          <p class="lg:text-[35.33px] text-[30px] text-center font-semibold dark:text-white">
+            Welcome to Rafikey!
+          </p>
+        </div>
+        <div class="flex justify-center">
+          <img src="@/assets/images/rafikey-bot.png" alt="rafikey-image" class="lg:w-full w-1/2" />
+        </div>
+        <div class="xl:w-6/12 w-9/12">
+          <p class="text-small text-gray-700 dark:text-white text-center flex justify-center">
+            Rafikey is your safe, private space to talk, learn and ask about anything related to your
+            body, relationship, mental health and more.
+          </p>
+        </div>
+        <AuthLoadingButtons />
       </div>
-      <div>
-        <img src="@/assets/images/rafikey-bot.png" alt="rafikey-image"  class=""/>
-      </div>
-      <div class="xl:w-6/12 w-9/12">
-        <p class="text-small text-gray-700 text-center flex justify-center ">
-          Rafikey is your safe, private space to talk, learn and ask about anything related to your
-          body, relationship, mental health and more.
-        </p>
-      </div>
-      <div class="flex space-x-2">
-        <div class="w-8 h-3 rounded-md animate-pulse bg-casablanca-400"></div>
-        <div class="w-3 h-3 rounded-full animate-pulse bg-casablanca-200"></div>
-        <div class="w-3 h-3 rounded-full animate-pulse bg-casablanca-300"></div>
+      <div  @click="moveNext" class="cursor-pointer dark:bg-darkgray flex  absolute right-1  top-1/2  rounded-sm -translate-y-1/2">
+        <span class="material-icons-outlined dark:text-white">keyboard_arrow_right</span>
       </div>
     </div>
+
+
   </div>
 </template>
 
