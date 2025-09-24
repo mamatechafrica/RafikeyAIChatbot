68de2e8dad0a435929752b4ef0ea5eab3564365e
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-09-24 16:40:22 +0300

    add GameResult

---
+++ rafikey-frontend/src/views/game/GameResult.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <div class="flex flex-col  justify-center h-[calc(100vh-10.5rem)] items-center">
+    <div class="flex justify-center ">
+      <img src="@/assets/images/video-game.png" alt="video-game-image" />
+    </div>
+    <div class="flex flex-col md:space-y-8 space-y-6 md:py-8 py-4 rounded-[14px] items-center bg-link-water-50 shadow-lg dark:bg-darkgray lg:w-1/3 md:w-2/3 w-full mx-auto " >
+      <div>
+        <p class="dark:text-white">Your score</p>
+      </div>
+      <div>
+        <img src="@/assets/images/confetti.png" alt="confetti-image" />
+      </div>
+      <div>
+        <p class="dark:text-white">4/5</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
