be8a73f8300f4f095ec242cb14976d6ca7bd8a41
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-09-24 16:40:04 +0300

    add backToChatHandler  function

--- rafikey-frontend/src/views/game/GamePage.vue
+++ rafikey-frontend/src/views/game/GamePage.vue
@@ -1,33 +1,37 @@
 <script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router'
 
-import { useRouter } from 'vue-router'
-
+const route = useRoute()
 const router = useRouter()
+
+const backToChatHandler = () => {
+  router.push({
+    name: 'newChat',
+  })
+}
 </script>
 
 <template>
-
-    <div class="dark:bg-lightgray h-screen pt-[38px]">
-      <div class="w-10/12 mx-auto space-y-4">
-        <div>
-          <button
-            @click="router.go(-1)"
-            class="btn btn-sm bg-transparent shadow-none border-[1px] dark:border-['#CBD5E1'] rounded-[10px] px-[8px] py-[6px]"
-          >
-            <span class="dark:text-white text-small">Back to chat</span>
-          </button>
-        </div>
-        <RouterView #default="{Component, route}">
-          <template v-if="Component">
-            <component :is="Component" :key="route.fullPath" />
-          </template>
-
-        </RouterView>
+  <div
+    class="dark:bg-lightgray min-h-screen"
+    :class="{ 'pt-[38px]': route.name != 'game-page-quiz-result' }"
+  >
+    <div class="w-10/12 mx-auto space-y-4">
+      <div class="sticky top-10">
+        <button
+          @click="backToChatHandler"
+          class="btn btn-sm bg-transparent shadow-none border-[1px] dark:border-['#CBD5E1'] rounded-[10px] px-[8px] py-[6px]"
+        >
+          <span class="dark:text-white text-small">Back to chat</span>
+        </button>
       </div>
-
-
+      <RouterView #default="{ Component, route }">
+        <template v-if="Component">
+          <component :is="Component" :key="route.fullPath" />
+        </template>
+      </RouterView>
+    </div>
   </div>
-
 </template>
 
 <style scoped></style>
