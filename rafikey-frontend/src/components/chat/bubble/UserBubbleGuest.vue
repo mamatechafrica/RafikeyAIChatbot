3ba5f496951c175631a6262eb7e751d6ca8a299c
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-02 14:37:52 +0300

    add UserBubbleGuest component

---
+++ rafikey-frontend/src/components/chat/bubble/UserBubbleGuest.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useColorGenerator } from '@/modules/colorGenerator.ts'
+
+export interface UserBubbleProps {
+  userName: string
+  userMessage: string
+  createdAt: string
+  isTyping?: boolean
+  isCopyable?: boolean
+  hasError?: boolean
+  picture?: string
+  isGeneratingResponse: boolean
+}
+
+const props = defineProps<UserBubbleProps>()
+
+const hasText = computed(() => {
+  return props.userMessage.length > 0
+})
+
+
+const { darkBgColor, bgColor, setColor} = useColorGenerator('ME')
+
+setColor()
+
+</script>
+
+<template>
+  <div class="chat chat-end">
+    <div class="flex flex-row-reverse gap-2">
+      <div>
+        <div
+          :class="[darkBgColor, bgColor ]"
+          class="rounded-full h-10 w-10 flex items-center justify-center font-bold"
+        >
+          <span class="dark:text-white">
+                ME
+            </span>
+          <!--          <img alt="user-avatar" src="@/assets/images/Avatar.png" />-->
+        </div>
+      </div>
+      <div class="flex flex-col">
+        <div class="chat-header dark:text-white flex justify-end items-end text-small">
+          {{ props.userName }}
+          <time class="text-extra-small opacity-50 flex">{{ props.createdAt }}</time>
+        </div>
+        <div class="bg-lightBackground dark:bg-bubbleDark p-4 rounded-2xl">
+          <div v-if="hasText" v-html="props.userMessage" class="text-small dark:text-white"></div>
+        </div>
+      </div>
+    </div>
+    <!--    <div class="chat-footer opacity-50">Seen at 12:46</div>-->
+    <!--    <p>Hello</p>-->
+  </div>
+</template>
+
+<style scoped></style>
