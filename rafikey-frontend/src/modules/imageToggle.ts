4004444365cf5c6fb9f00218b7f736a08c29fcde
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-02 14:49:07 +0300

    add image files

--- rafikey-frontend/src/modules/imageToggle.ts
+++ rafikey-frontend/src/modules/imageToggle.ts
@@ -1,5 +1,7 @@
-import ImageDark from '@/assets/images/rafikey-icon-dark.png'
-import ImageLight from '@/assets/images/rafikey-icon-light.png'
+import ImageDark from '@/assets/images/Rafikey_D-mode_Image.png'
+import ImageLight from '@/assets/images/Rafikey_L-mode_Image.png'
+import ImageSmallLight from '@/assets/images/Rafikey L-mode-small_Image.png'
+import ImageSmallLDark from '@/assets/images/Rafikey D-mode-small_Image.png'
 import { useRafikeyChatbotStore } from '@/stores'
 const chatbotStore = useRafikeyChatbotStore()
 
