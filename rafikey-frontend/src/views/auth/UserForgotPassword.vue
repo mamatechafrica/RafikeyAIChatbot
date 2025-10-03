942110ccc27f6eb7a71cb0ed985d9a0c5680e582
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-03 08:44:19 +0300

    add imageToggleSmallDevice function

--- rafikey-frontend/src/views/auth/UserForgotPassword.vue
+++ rafikey-frontend/src/views/auth/UserForgotPassword.vue
@@ -2,12 +2,11 @@
 import { ref, watch, computed, reactive } from 'vue'
 import { useField } from 'vee-validate'
 import { useRouter } from 'vue-router'
-import { useAuthStore, useRafikeyChatbotStore } from '@/stores'
+import { useAuthStore } from '@/stores'
 import { showSweetAlert } from '@/modules/alert.ts'
 // import imageLight from '@/assets/images/rafikey-icon-light.png'
 // import imageDark from '@/assets/images/rafikey-icon-dark.png'
-import { toggleImage} from '@/modules/imageToggle.ts'
-
+import { imageToggleSmallDevice, toggleImage } from '@/modules/imageToggle.ts'
 
 const router = useRouter()
 const emailData = ref<string>('')
@@ -17,8 +16,6 @@ const forgotPasswordError = reactive({
   isError: false,
   message: '',
 })
-
-
 
 // const toggleImage = computed(() => {
 //   console.log('Toggle', chatbotStore.isDarkMode)
@@ -114,14 +111,14 @@ const forgotPasswordHandler = () => {
             @click="router.go(-1)"
             class="flex cursor-pointer text-gray-1000 justify-start w-fit hover:bg-transparent border-none hover:border-none btn btn-sm btn-ghost shadow-none"
           >
-            <span class="material-icons-outlined dark:text-white ">chevron_left</span>
-            <span class="text-extra-small font-light dark:text-white ">Back to login</span>
+            <span class="material-icons-outlined dark:text-white">chevron_left</span>
+            <span class="text-extra-small font-light dark:text-white">Back to login</span>
           </div>
           <div class="flex flex-col space-y-6">
-            <h2 class="text-extra-extra-large font-semibold dark:text-white ">
+            <h2 class="text-extra-extra-large font-semibold dark:text-white">
               Forgot your password?
             </h2>
-            <p class="text-large text-gray-700  dark:text-white  ">
+            <p class="text-large text-gray-700 dark:text-white">
               Don't worry, happens to all of us. Enter your email bellow to recover your password
             </p>
           </div>
@@ -137,11 +134,11 @@ const forgotPasswordHandler = () => {
                   id="email"
                   type="email"
                   required
-                  class="w-full dark:bg-darkgray dark:text-white border-[1px] border-gray-300 rounded-2xl p-4 text-small "
+                  class="w-full dark:bg-darkgray dark:text-white border-[1px] border-gray-300 rounded-2xl p-4 text-small"
                   placeholder="johndoe@gmail.com"
                 />
                 <label
-                  class="absolute left-1 -top-3 dark:text-white dark:bg-darkgray text-slate-600 px-1 text-text-extra-small bg-white "
+                  class="absolute left-1 -top-3 dark:text-white dark:bg-darkgray text-slate-600 px-1 text-text-extra-small bg-white"
                   for="email"
                 >
                   Email address
@@ -167,7 +164,7 @@ const forgotPasswordHandler = () => {
       </div>
       <!--      right--side-->
       <div class="col-span-1 flex items-center justify-center">
-        <div class="bg-gray-100 dark:bg-lightgray rounded-2xl  h-5/6">
+        <div class="bg-gray-100 dark:bg-lightgray rounded-2xl h-5/6">
           <img src="@/assets/images/forgot-password.png" alt="forgot-password-image" />
         </div>
       </div>
@@ -181,25 +178,25 @@ const forgotPasswordHandler = () => {
         @click="router.go(-1)"
         class="col-span-1 flex cursor-pointer text-gray-1000 justify-start w-fit hover:bg-transparent border-none hover:border-none btn btn-sm btn-ghost shadow-none"
       >
-        <span class="material-icons-outlined dark:text-white ">chevron_left</span>
-        <span class="text-extra-small font-light dark:text-white  ">Back</span>
+        <span class="material-icons-outlined dark:text-white">chevron_left</span>
+        <span class="text-extra-small font-light dark:text-white">Back</span>
       </div>
 
       <div class="col-span-1 flex justify-center">
-        <div class="w-54   flex justify-center ">
-          <img src="@/assets/images/rafikey-key.png" alt="rafikey-logo"  />
+        <div class="w-44 h-24 mx-auto flex justify-center items-center">
+          <img :src="imageToggleSmallDevice()" alt="rafikey-logo" />
         </div>
       </div>
     </div>
 
     <div class="flex flex-col md:px-20 px-10 space-y-6">
       <div class="flex flex-col items-center space-y-4">
-        <h2 class="text-extra-large font-semibold dark:text-white ">Forgot Password</h2>
+        <h2 class="text-extra-large font-semibold dark:text-white">Forgot Password</h2>
         <div class="text-extra-small md:space-y-2 space-y-0.5">
-          <p class=" text-gray-700 text-center dark:text-white ">
+          <p class="text-gray-700 text-center dark:text-white">
             Don't worry. Happens to all of us.
           </p>
-          <p class="text-gray-700 text-center dark:text-white ">
+          <p class="text-gray-700 text-center dark:text-white">
             Enter your Email below to recover your password
           </p>
         </div>
@@ -207,12 +204,12 @@ const forgotPasswordHandler = () => {
       <div class="border-b border-gray-400 w-full"></div>
       <form class="space-y-8">
         <div class="space-y-3">
-          <label for="email" class="dark:text-white text-black text-extra-small ">Email</label>
+          <label for="email" class="dark:text-white text-black text-extra-small">Email</label>
           <input
             v-model="emailData"
             id="email"
             type="email"
-            class="w-full dark:bg-darkgray dark:text-white   border-[1px] border-gray-300  rounded-2xl  md:p-4 p-3 text-extra-small "
+            class="w-full dark:bg-darkgray dark:text-white border-[1px] border-gray-300 rounded-2xl md:p-4 p-3 text-extra-small"
             required
             placeholder="maryjane@gmail.com"
           />
@@ -235,12 +232,12 @@ const forgotPasswordHandler = () => {
           <span v-if="!isLoading" class="text-extra-small">Submit</span>
           <span v-else class="loading loading-spinner loading-sm"></span>
         </button>
-<!--        <p class="text-center text-slate-800 dark:text-white text-extra-small">-->
-<!--          Don't have an account?-->
-<!--          <router-link to="/auth/register" class="text-rose-400 text-extra-small">-->
-<!--            Sign Up-->
-<!--          </router-link>-->
-<!--        </p>-->
+        <!--        <p class="text-center text-slate-800 dark:text-white text-extra-small">-->
+        <!--          Don't have an account?-->
+        <!--          <router-link to="/auth/register" class="text-rose-400 text-extra-small">-->
+        <!--            Sign Up-->
+        <!--          </router-link>-->
+        <!--        </p>-->
       </div>
     </div>
   </div>
