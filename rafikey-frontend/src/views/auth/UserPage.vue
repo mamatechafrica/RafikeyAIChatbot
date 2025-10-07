cedddb786289f9c2215ac6d943f534316083c8ec
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-07 15:49:44 +0300

    add notification for virtual-pwa module hooks

--- rafikey-frontend/src/views/auth/UserPage.vue
+++ rafikey-frontend/src/views/auth/UserPage.vue
@@ -1,4 +1,47 @@
 <script setup lang="ts">
+import { usePWA } from  '@/composables/usePWA.ts'
+import  {ref, watch} from "vue";
+import DialogModal from '@/components/DialogModal.vue'
+import { useNotificationStore } from '@/stores'
+const {needRefresh, offlineReady,   updateSW} = usePWA()
+
+
+const notificationStore = useNotificationStore()
+const isShowDialog = ref(false)
+
+watch(offlineReady, (ready) => {
+  if (ready) {
+    console.log("Offline is ready")
+    notificationStore.addNotification('App ready to work offline', 'info')
+  }
+})
+
+watch(needRefresh, (value)=>{
+  if(value){
+    isShowDialog.value = true
+  }
+})
+
+
+const dontUpdateAppHandler = () =>{
+  console.log("Don't update handler called")
+  needRefresh.value = false
+  updateSW(false)
+  isShowDialog.value = false
+}
+
+const isLoadingUpdates = ref(false)
+
+const updateAppHandler = () =>{
+  updateSW(true)
+  isLoadingUpdates.value = true
+
+  setTimeout(()=>{
+    isShowDialog.value = false
+    window.location.reload()
+  }, 2000)
+}
+
 
 </script>
 
