acbf1c9ebf0a6a9dbdc15a2c0056078a1b4f6fce
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-07 15:50:28 +0300

    add usePWA composable

---
+++ rafikey-frontend/src/composables/usePWA.ts
@@ -0,0 +1,25 @@
+import { ref } from 'vue'
+import { registerSW } from 'virtual:pwa-register'
+
+
+export function usePWA (){
+  const needRefresh  = ref(false)
+  const offlineReady = ref(false)
+
+  const updateSW = registerSW(
+    {
+      onOfflineReady(){
+        offlineReady.value = true
+      },
+      onNeedRefresh(){
+        needRefresh.value = true
+      }
+    }
+  )
+
+  return {
+    needRefresh,
+    offlineReady,
+    updateSW
+  }
+}
