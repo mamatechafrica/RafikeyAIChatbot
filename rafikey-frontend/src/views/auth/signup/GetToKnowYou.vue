a9b2161c66eb61ec81facba1ce13ca861902873e
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-08 08:12:15 +0300

    add styling

--- rafikey-frontend/src/views/auth/signup/GetToKnowYou.vue
+++ rafikey-frontend/src/views/auth/signup/GetToKnowYou.vue
@@ -17,10 +17,6 @@ const authStore = useAuthStore()
 const notificationStore = useNotificationStore()
 const router = useRouter()
 const isLoading = ref(false)
-
-// const toggleImage = computed(() => {
-//   return chatbotStore.isDarkMode ? imageDark : imageLight
-// })
 const ageRangePlaceholder = 'Choose your age range'
 const genderPlaceholder = 'Choose your gender'
 const relationshipPlaceholder = 'Pick an option'
@@ -164,29 +160,29 @@ onBeforeMount(() => {
 </script>
 
 <template>
-  <div class="min-h-screen dark:bg-lightgray w-full py-6 md:flex ">
+  <div class="min-h-screen dark:bg-lightgray w-full py-6 md:flex">
     <div
-      class="relative bg-lightBackground dark:bg-darkgray  flex flex-col w-10/12 mx-auto rounded-[34px] pb-10"
+      class="relative bg-lightBackground dark:bg-darkgray flex flex-col w-10/12 mx-auto rounded-[34px] pb-10"
     >
       <div>
-        <div class=" h-20 flex justify-start items-center">
-          <img :src='toggleImage()' alt="rafikey-logo" class="w-40 pt-6 ps-4"  />
+        <div class="h-20 flex justify-start items-center">
+          <img :src="toggleImage()" alt="rafikey-logo" class="w-40 pt-6 ps-4" />
         </div>
         <div class="w-11/12 space-y-12 rounded-2xl mx-auto">
           <div class="md:pt-14 pt-1">
             <div class="flex flex-col items-center">
-              <h2 class="text-extra-extra-large dark:text-white font-semibold ">
+              <h2 class="text-extra-extra-large dark:text-white font-semibold">
                 Let's Get To Know You Better
               </h2>
             </div>
           </div>
 
-          <div class="border-b border-gray-400 w-1/3 mx-auto "></div>
-          <div class="space-y-6 ">
+          <div class="border-b border-gray-400 w-1/3 mx-auto"></div>
+          <div class="space-y-6">
             <div class="space-y-4">
               <div class="flex flex-col items-center">
-                <p class="text-small dark:text-white ">How old are you (Required)</p>
-                <span class="text-small text-gray-700 dark:text-stone-300 "
+                <p class="text-small dark:text-white">How old are you (Required)</p>
+                <span class="text-small text-gray-700 dark:text-stone-300"
                   >So we can tailor content and support</span
                 >
               </div>
@@ -197,7 +193,7 @@ onBeforeMount(() => {
 
             <div class="space-y-4">
               <div class="flex justify-center">
-                <p class="text-small dark:text-white ">Which gender are you (Required)</p>
+                <p class="text-small dark:text-white">Which gender are you (Required)</p>
               </div>
               <div class="">
                 <RadioGroup :radio-type="genderSet" @change="selectedRadio" />
@@ -206,7 +202,7 @@ onBeforeMount(() => {
             <div class="space-y-4">
               <div class="flex justify-center flex-col items-center">
                 <p class="text-small dark:text-white">Are you in a relationship? (Required)</p>
-                <span class="text-small text-gray-700 dark:text-stone-300 "
+                <span class="text-small text-gray-700 dark:text-stone-300"
                   >Helps with personalized advice</span
                 >
               </div>
@@ -215,20 +211,20 @@ onBeforeMount(() => {
                 <RadioGroup :radio-type="relationshipStatus" @change="selectedRadio" />
               </div>
             </div>
-            <div
-              v-if="signupError.isError"
-              class="flex gap-2 justify-start w-full lg:w-3/4 xl:w-1/2 mx-auto"
-            >
-              <span class="material-icons-outlined text-rose-500">error</span>
-              <span class="text-rose-500">{{ signupError.message }}</span>
-            </div>
+                        <div
+                          v-if="signupError.isError"
+                          class="flex gap-2 justify-start w-full lg:w-3/4 xl:w-1/2 mx-auto"
+                        >
+                          <span class="material-icons-outlined text-rose-500">error</span>
+                          <span class="text-rose-500">{{ signupError.message }}</span>
+                        </div>
           </div>
-          <div class="border-b border-gray-400 w-1/3 mx-auto "></div>
+          <div class="border-b border-gray-400 w-1/3 mx-auto"></div>
           <div class="flex justify-center">
             <button
               @click="createAccountHandler"
-              :class="[!everyThingOk? 'bg-button-inactive' : 'bg-button-light']"
-              class="btn w-1/3 btn-sm text-lg py-6 rounded-2xl  border-none shadow-none"
+              :class="[!everyThingOk ? 'bg-button-inactive' : 'bg-button-light']"
+              class="btn w-1/3 btn-sm text-lg py-6 rounded-2xl border-none shadow-none"
             >
               <span v-if="!isLoading" class="text-extra-small">Sign up</span>
               <span v-else class="loading loading-spinner loading-sm"></span>
@@ -240,31 +236,31 @@ onBeforeMount(() => {
   </div>
 
   <!--    Small screen -->
-  <div class="md:hidden dark:bg-darkgray block min-h-screen w-full overflow-hidden ">
-    <div class=" w-36 h-20 mx-auto flex justify-center items-center">
+  <div class="md:hidden dark:bg-darkgray block min-h-screen w-full overflow-hidden">
+    <div class="w-36 h-20 mx-auto flex justify-center items-center">
       <img :src="imageToggleSmallDevice()" alt="rafikey-logo" />
     </div>
     <div class="md:px-20 px-6 space-y-4 sm:space-y-6">
       <div>
-        <h2 class="text-extra-large dark:text-white font-semibold text-center ">
+        <h2 class="text-extra-large dark:text-white font-semibold text-center">
           Let's Know You Better
         </h2>
       </div>
-      <div class=" border-b-[1px] border-stone-400  smx-auto "></div>
+      <div class="border-b-[1px] border-stone-400 smx-auto"></div>
 
       <div class="space-y-2">
-        <p class="text-gray-950 dark:text-white  text-extra-small">Age Range (Required)</p>
+        <p class="text-gray-950 dark:text-white text-extra-small">Age Range (Required)</p>
         <LisxBox
           :list-items="ageRange"
           :place-holder="ageRangePlaceholder"
           @selected-list-item="selectedRadio"
         />
-        <p class="text-gray-400 dark:text-stone-300 text-extra-extra-small ">
+        <p class="text-gray-400 dark:text-stone-300 text-extra-extra-small">
           This helps us give you the right kind of support and info for your age
         </p>
       </div>
       <div class="space-y-2">
-        <p class="text-gray-950 text-extra-small dark:text-white ">Gender (Required)</p>
+        <p class="text-gray-950 text-extra-small dark:text-white">Gender (Required)</p>
         <LisxBox
           :list-items="genderSet"
           :place-holder="genderPlaceholder"
@@ -275,24 +271,26 @@ onBeforeMount(() => {
         </p>
       </div>
       <div class="space-y-2">
-        <p class="text-gray-950 text-extra-small dark:text-white">Are you in a relationship (Required)</p>
+        <p class="text-gray-950 text-extra-small dark:text-white">
+          Are you in a relationship (Required)
+        </p>
         <LisxBox
           :list-items="relationshipStatus"
           :place-holder="relationshipPlaceholder"
           @selected-list-item="selectedRadio"
         />
-        <p class="text-gray-400 text-extra-extra-small ">To proceed fill all the fields</p>
+        <p class="text-gray-400 text-extra-extra-small">To proceed fill all the fields</p>
       </div>
-      <div v-if="signupError.isError" class="flex gap-2">
-        <span class="material-icons-outlined text-rose-500">error</span>
-        <span class="text-rose-500 md:text-lg text-sm">{{ signupError.message }}</span>
-      </div>
-      <div class="border-b-[1px] border-stone-400 mx-auto "></div>
-      <div class="w-3/4 mx-auto  flex justify-between items-center">
+            <div v-if="signupError.isError" class="flex gap-2">
+              <span class="material-icons-outlined text-rose-500">error</span>
+              <span class="text-rose-500 md:text-lg text-sm">{{ signupError.message }}</span>
+            </div>
+      <div class="border-b-[1px] border-stone-400 mx-auto"></div>
+      <div class="w-3/4 mx-auto flex justify-between items-center">
         <button
           @click="createAccountHandler"
-          :class="[!everyThingOk? 'bg-button-inactive' : 'bg-button-light']"
-          class="btn w-full btn-sm 0 text-lg rounded-2xl py-5  border-none shadow-none"
+          :class="[!everyThingOk ? 'bg-button-inactive' : 'bg-button-light']"
+          class="btn w-full btn-sm 0 text-lg rounded-2xl py-5 border-none shadow-none"
         >
           <span v-if="!isLoading" class="text-sm md:text-lg">Sign up</span>
           <span v-else class="loading loading-spinner loading-sm"></span>
