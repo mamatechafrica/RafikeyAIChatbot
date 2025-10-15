6d82d51db63530d48b6a38cfd7bbb7fe31c20b9d
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-15 13:31:05 +0300

    change in service worker

--- rafikey-frontend/dev-dist/sw.js
+++ rafikey-frontend/dev-dist/sw.js
@@ -81,8 +81,11 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
+    "url": "registerSW.js",
+    "revision": "3ca0b8505b4bec776b69afdba2768812"
+  }, {
     "url": "index.html",
-    "revision": "0.lip1ka7lnqo"
+    "revision": "0.ov7of1iafao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
