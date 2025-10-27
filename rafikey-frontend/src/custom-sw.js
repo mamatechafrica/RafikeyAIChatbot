e103091fb8d130e43a7d221e26d123178c33362a
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-27 19:17:59 +0300

    add custom service worker file for listen push even from backend

---
+++ rafikey-frontend/src/custom-sw.js
@@ -0,0 +1,42 @@
+/* eslint-env serviceworker */
+
+// self.__WB_MANIFEST
+
+import { precacheAndRoute } from 'workbox-precaching'
+// eslint-disable-next-line no-undef
+precacheAndRoute(self.__WB_MANIFEST)
+
+
+// eslint-disable-next-line no-undef
+self.addEventListener('push', (event)=> {
+  // eslint-disable-next-line no-undef
+  console.log('SW push received', event.data.json())
+
+  let data = {}
+  if (event.data){
+    try {
+      data = event.data.json()
+    } catch(err){
+      //   If plain text instead of a JSON
+      data = { title: 'Chat Clean up Notice ', body: event.data.text()}
+    }
+  }
+
+  const title = data.title ||  '⚠️ Notice'
+  const options = {
+    body: data.body || 'Please be notified that chats at least 30days old will be deleted in less than 24 hours',
+    icon: '/favicon.png',
+    // vibrate: [100, 50, 100],
+    // data: { dateOfArrival: Date.now(), primaryKey: 1 },
+    // actions: [
+    //   { action: 'explore', title: 'Go to the site' },
+    //   { action: 'close', title: 'Close the notification' },
+    // ],
+  }
+  event.waitUntil(
+    // eslint-disable-next-line no-undef
+    self.registration.showNotification(title, options)
+  )
+})
+
+
