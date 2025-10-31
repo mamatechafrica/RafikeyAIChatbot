2f4d5bfb9d3f863b08ae7c84c3fafbbd97d07985
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-31 23:02:00 +0300

    remove the quick access buttons

--- rafikey-frontend/src/views/chatbot/user/NewChat.vue
+++ rafikey-frontend/src/views/chatbot/user/NewChat.vue
@@ -170,7 +170,7 @@ const goToServiceFInder = () =>{
             <SRHRGameButton @close-game-button="closeGameButton" />
           </div>
 
-          <div class="lg:col-span-1 flex justify-end col-span-12">
+          <div class="flex justify-end lg:justify-start w-full lg:w-auto">
             <img
               src="../../../assets/images/rafiikey-mascot2.svg"
               alt="rafikey-mascot"
@@ -187,11 +187,6 @@ const goToServiceFInder = () =>{
           <img :src="toggleImage()" alt="rafikey-icon" />
         </div>
         <div class="flex items-center justify-between gap-6">
-          <!--          <div-->
-          <!--            class="border dark:border-stone-300 rounded-full flex h-6 w-6 justify-center items-center"-->
-          <!--          >-->
-          <!--            <span class="material-icons-outlined dark:text-stone-300 !text-lg">settings</span>-->
-          <!--          </div>-->
           <div>
             <div
               :class="[darkBgColor, bgColor]"
@@ -200,55 +195,52 @@ const goToServiceFInder = () =>{
               <span class="dark:text-white text-sm">{{
                 JSON.parse(authStore.user).username.substring(0, 2).toUpperCase()
               }}</span>
-              <!--          <img alt="user-avatar" src="@/assets/images/Avatar.png" />-->
             </div>
           </div>
         </div>
       </div>
 
       <div class="flex gap-2">
-        <!--        <div-->
-        <!--          @click.stop="startChatHandler"-->
-        <!--          class="cursor-pointer w-full dark:bg-darkgray bg-link-water-50 rounded-xl sm:p-5 p-3 sm:space-y-4 space-y-2"-->
-        <!--        >-->
-        <!--          <div class="bg-purple-500 rounded-full h-10 w-10 flex justify-center items-center">-->
-        <!--            <span class="material-icons-outlined dark:text-white">sms</span>-->
-        <!--          </div>-->
-        <!--          <div class="flex dark:text-white sm:gap-4  justify-between">-->
-        <!--            <p class="text-small">Chat with Rafikey</p>-->
-        <!--            <span class="material-icons-outlined text-sm">arrow_forward</span>-->
-        <!--          </div>-->
-        <!--        </div>-->
         <div
           @click.stop="goToPlayPage"
           class="cursor-pointer w-full dark:bg-darkgray bg-link-water-50 rounded-xl sm:p-5 p-3 sm:space-y-4 space-y-2"
         >
           <div class="bg-casablanca-300 rounded-full h-10 w-10 flex justify-center items-center">
-            <!--            <span class="material-icons-outlined dark:text-white">mic_none</span>-->
             <img src="@/assets/images/game.png" alt="game-image" />
           </div>
-          <div class="flex dark:text-white gap-4">
-            <p class="text-small">Play games</p>
+          <div class="flex dark:text-white justify-between gap-4">
+            <p class="text-small text-nowrap">Play games</p>
+            <span class="material-icons-outlined text-sm">arrow_forward</span>
+          </div>
+        </div>
+        <div
+          @click.stop="goToServiceFInder "
+          class="cursor-pointer w-full dark:bg-darkgray bg-link-water-50 rounded-xl sm:p-5 p-3 sm:space-y-4 space-y-2"
+        >
+          <div :class="[darkBgColor]" class=" rounded-full h-10 w-10 flex justify-center items-center">
+            <span class="material-icons-outlined">health_and_safety</span>
+          </div>
+          <div class="flex dark:text-white justify-between gap-4">
+            <p class="text-small text-nowrap">Find a service</p>
             <span class="material-icons-outlined text-sm">arrow_forward</span>
           </div>
         </div>
       </div>
 
-      <div class="space-y-2 pt-10">
-        <div class="flex justify-between sm:p-4">
-          <p class="dark:text-white text-extra-large">Quick Access</p>
-          <!--          <span class="text-purple-400 text-small">See all</span>-->
-        </div>
-        <div v-for="qn in accessQuestions" :key="qn.id">
-          <div
-            @click="accessButtonQuestionHandler(qn.question)"
-            class="cursor-pointer flex dark:bg-darkgray bg-link-water-50 rounded-xl sm:p-5 p-3 gap-4"
-          >
-            <span class="material-icons-outlined dark:text-white">{{ qn.icon }}</span>
-            <p class="dark:text-white text-small">{{ qn.question }}</p>
-          </div>
-        </div>
-      </div>
+<!--      <div class="space-y-2 pt-10">-->
+<!--        <div class="flex justify-between sm:p-4">-->
+<!--          <p class="dark:text-white text-extra-large">Quick Access</p>-->
+<!--        </div>-->
+<!--        <div v-for="qn in accessQuestions" :key="qn.id">-->
+<!--          <div-->
+<!--            @click="accessButtonQuestionHandler(qn.question)"-->
+<!--            class="cursor-pointer flex dark:bg-darkgray bg-link-water-50 rounded-xl sm:p-5 p-3 gap-4"-->
+<!--          >-->
+<!--            <span class="material-icons-outlined dark:text-white">{{ qn.icon }}</span>-->
+<!--            <p class="dark:text-white text-small">{{ qn.question }}</p>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
     </div>
   </div>
 </template>
